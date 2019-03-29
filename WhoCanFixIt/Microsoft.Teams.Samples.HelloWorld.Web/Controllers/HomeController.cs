c39f1d19284db4c0b212da8ac2d2c152a2b0aa43
Vincent Schmieder <v.schmieder@gmx.de>
2019-03-29 22:33:24 +0100

    Initial

---
+++ WhoCanFixIt/Microsoft.Teams.Samples.HelloWorld.Web/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hello")]
+        public ActionResult Hello()
+        {
+            return View("Index");
+        }
+
+        [Route("first")]
+        public ActionResult First()
+        {
+            return View();
+        }
+
+        [Route("second")]
+        public ActionResult Second()
+        {
+            return View();
+        }
+
+        [Route("configure")]
+        public ActionResult Configure()
+        {
+            return View();
+        }
+        
+        public static async Task MakePredictionRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid Prediction-Key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "3b9dde6d1ae1453a86bfeb1d945300f2");
+
+            // Prediction URL - replace this example URL with your valid Prediction URL.
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/8622c779-471c-4b6e-842c-67a11deffd7b/classify/iterations/Cats%20vs.%20Dogs%20-%20Published%20Iteration%203/image";
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                //Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        private static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
